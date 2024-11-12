962f3f531e5cc88bff094431611bb6a027c8581a
Daniel Martinez <daniel@houlak.com>
2024-11-12 14:57:28 -0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Daniel Martinez Condinanza on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
